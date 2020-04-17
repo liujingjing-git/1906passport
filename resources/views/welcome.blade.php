88191c0e89bb2f74cf90c1e16db6be6c30c75ed2
liu <liu@localhost.localdomain>
2020-04-17 02:54:20 +0000

    更改

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   PassPort
                 </div>
                 <hr>
                 <div class="links">
